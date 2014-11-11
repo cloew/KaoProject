6798e61958c2b9b08a8bc3f020804a8c14502bd6
Chris Loew <cloew123@gmail.com>
2014-11-10 19:35:37 -0700

    Added basic setup script

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='kao_project',
+      version='.1',
+      #description='Kao Tessur Deck Package',
+      author='Chris Loew',
+      author_email='cloew123@gmail.com',
+      #url='http://www.python.org/sigs/distutils-sig/',
+      packages=['kao_project'],
+     )
