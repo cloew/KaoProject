ce45c15738f1a8b14a38f0a2593c171ecf862424
Chris Loew <cloew123@gmail.com>
2014-11-11 20:07:26 -0700

    Git Extension Initialize now returns whether the project changed

--- kao_project/extension/git_extension.py
+++ kao_project/extension/git_extension.py
@@ -15,12 +15,22 @@ class GitExtension:
         os.chdir(parent.filepath)
         subprocess.call(['git', 'init'])
         subprocess.call(['git', 'remote', 'add', 'origin', self.url])
-        subprocess.call(['git', 'pull', 'origin', 'master'])
+        pulledData = self.pull(['git', 'pull', 'origin', 'master'])
         subprocess.call(['git', 'push', '-u', 'origin', 'master'])
+        return pulledData
         
     def update(self, parent):
         """ Update the git project """
         os.chdir(parent.filepath)
-        process = subprocess.Popen(['git', 'pull'], stdout=subprocess.PIPE)
+        return self.pull(['git', 'pull'])
+        
+    def onChange(self):
+        """ Do nothing when the project has changed """
+        pass
+        
+    def pull(self, command):
+        """ Pull from the origin """
+        process = subprocess.Popen(command, stdout=subprocess.PIPE)
         output = process.communicate()[0]
         return process.returncode == 0 and self.NO_NEW_DATA != output.strip()
+        
