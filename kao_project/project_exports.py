ac7c47ad48edc294659434b8373a78dc66951c04
Chris Loew <cloew123@gmail.com>
2014-11-17 19:21:42 -0700

    Now export environment variables for the project and project src directories

--- kao_project/project_exports.py
+++ kao_project/project_exports.py
@@ -8,12 +8,32 @@ class ProjectExports:
     
     def getLines(self):
         """ Return the export and alias lines """
-        return [self.header, self.goToAlias]
+        return [self.header, self.projectDir, self.projectSrcDir, self.goToAlias]
         
     @property
     def header(self):
         """ Return the Exports header """
         return "# {0} Commands".format(self.project.name)
+        
+    @property
+    def projectDir(self):
+        """ Return the project directory """
+        return 'export {0}={1}'.format(self.projectDirVar, self.filepath)
+        
+    @property
+    def projectSrcDir(self):
+        """ Return the project source directory """
+        return 'export {0}={1}'.format(self.projectSrcDirVar, self.srcpath)
+        
+    @property
+    def projectDirVar(self):
+        """ Return the project directory variable """
+        return '{0}_dir'.format(self.commandName)
+        
+    @property
+    def projectSrcDirVar(self):
+        """ Return the project source directory variable """
+        return '{0}_src'.format(self.commandName)
         
     @property
     def goToAlias(self):
