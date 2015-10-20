0ff5324550104c549bf8bafe441bea63c71ba9bf
Chris Loew <cloew123@gmail.com>
2015-10-20 10:53:25 -0600

    Fixed bug

--- kao_project/project_factory.py
+++ kao_project/project_factory.py
@@ -8,7 +8,7 @@ from kao_factory.Source.json_source import JsonSource
 import os
 
 PROJECTS_FILENAME = os.path.expanduser("~/.projects.json")
-PathFactory = Factory(Path, FieldArg("envVar"), FieldArg("path"), srcPath=FieldArg("srcPath")])
+PathFactory = Factory(Path, FieldArg("envVar"), FieldArg("path"), srcPath=FieldArg("srcPath"))
 
 parameters = [PrimitiveParameter("name"),
               PathFactory.LoadArg("path"),
